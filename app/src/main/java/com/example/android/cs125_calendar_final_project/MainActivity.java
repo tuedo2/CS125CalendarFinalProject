51bf4d3085882b6d377d819026a88a03528f22a8
tuedo <tuedo2@illinois.edu>
2019-04-23 18:16:11 -0500

    Initial commit

---
+++ app/src/main/java/com/example/android/cs125_calendar_final_project/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.android.cs125_calendar_final_project;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
