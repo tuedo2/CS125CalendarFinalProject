fba75af30aec41eaa7eb5ddd0e7f98314e01f0d2
tuedo <tuedo2@illinois.edu>
2019-04-24 18:45:49 -0500

    Unfunctional Template (CalendarView) - Tue

--- app/src/main/java/com/example/android/cs125_calendar_final_project/MainActivity.java
+++ app/src/main/java/com/example/android/cs125_calendar_final_project/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.android.cs125_calendar_final_project;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,6 +10,12 @@ import android.widget.CalendarView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+/**
+ * Here's the idea: you can click on one of the dates in mainView (the calendar) and then each day
+ * has it's own editable text. When you click the "save" button the text is saved for that day and
+ * you can reaccess it. That's pretty much it.
+ */
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -16,11 +23,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         /**
-         * CalendarView stuff.
+         * Widget stuff.
          */
         CalendarView mainView = findViewById(R.id.mainView);
         TextView eventText = findViewById(R.id.eventList);
-        eventText.setFocusable(true);
+        Button saveButton = findViewById(R.id.saveButton);
+
+        /**
+         * Default stuff.
+         */
+        eventText.setFocusable(true); // Making the textView editable
         eventText.setEnabled(true);
         eventText.setClickable(true);
         eventText.setFocusableInTouchMode(true);
